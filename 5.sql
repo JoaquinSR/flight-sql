b354ef5e58b7dad5953d5deeb3644f8e28e12d78
JoaquinSR <JoaquinSR@users.noreply.github.com>
2021-05-04 12:36:59 +0200

    fix ex5

--- 5.sql
+++ 5.sql
@@ -1,2 +1,2 @@
 SELECT UniqueCarrier, colYear, colMonth, SUM(Cancelled) AS "total_cancelled"
-FROM flights GROUP BY UniqueCarrier, colYear, colMonth ORDER BY total_cancelled DESC;
+FROM flights  GROUP BY UniqueCarrier, colYear, colMonth HAVING total_cancelled > 0 ORDER BY total_cancelled DESC;
